fcbd333c93cf0c57d87e85c384410f7c52ac4f6d
th3m3ch4n1c <128443877+augustinemkii@users.noreply.github.com>
2023-10-15 18:51:23 +0530

    Update mirror_file.sh

--- mirror_file.sh
+++ mirror_file.sh
@@ -2,15 +2,13 @@
 
 ##script to mirror the contents of the file 
 
-echo "which file to mirror: "
-read var
-echo "output to: (give the file name with extension)"
-read var2
-while read line
-do
-	#echo "ouput file: "
-	#read var2
-	echo $line  >> $var2
-done  < $var
+echo "which file to mirror: "  ##lets user input the target file for mirroring its contents
+read var   ##given input is written into the variable named var
+echo "output to: (give the file name with extension)"  ##lets user input the output format of the file 
+read var2  ##given input is written into the variable named var2
+while read line  ##while loop with the condition of the reading whatever fed to it and write that in variable named line
+do               ## this tells the interpreter to execute following block of the code till the while loop doesnt exits.
+	echo $line  >> $var2  ##this is to append the contents of the variable 'line' into the contents of the variable 'var2'
+done  < $var     ## tbh dont know what is going.
 
 
